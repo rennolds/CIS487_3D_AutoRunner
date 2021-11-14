d14324251c25bec3d0118522a66a09150a04ddfe
Tommy Rennolds <thomasrennolds@gmail.com>
2021-11-14 15:03:33 -0500

    basic movement and camera following implemented

---
+++ NewtonsFactory/Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); // adds a force of 2000 on the z-axis
+
+        if (Input.GetKey("d"))
+        {
+          rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+          rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
