2e1b3ce4197d4b068b3b24a17a4b9d489cec4781
Tommy Rennolds <thomasrennolds@gmail.com>
2021-12-07 14:46:38 -0500

    added to level

--- NewtonsFactory/Assets/Scripts/GameManager.cs
+++ NewtonsFactory/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class GameManager : MonoBehaviour
   {
     //player died or failed to reach peak velocity
     Debug.Log("GAME OVER");
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
   }
 
   public void LevelWon()
