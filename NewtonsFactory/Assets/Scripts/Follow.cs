8690c91ac8d1d9096b1287fdaf3d5be4c8794981
Tommy Rennolds <thomasrennolds@gmail.com>
2021-12-07 18:00:49 -0500

    test script

--- NewtonsFactory/Assets/Scripts/Follow.cs
+++ NewtonsFactory/Assets/Scripts/Follow.cs
@@ -7,10 +7,6 @@ public class Follow : MonoBehaviour
     public Transform player;
     public Vector3 offset;
     // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
