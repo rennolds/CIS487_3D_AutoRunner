047a6269db25ef4b56d26fda347c14d18ca06c72
nerd-of-now <68018081+nerd-of-now@users.noreply.github.com>
2021-12-08 12:10:58 -0500

    Success and Failure Screens added to SampleScene
    Ideally this should be set up in such a way that it can be copied to any scenes made.
    
    Things that aren't fully implemented:
    -Loading the Main Menu Scene
    -Loading the Next Level Scene (this would likely require a certain way of naming the levels such that it follows a pattern)

---
+++ NewtonsFactory/Assets/Scripts/FailureScreen.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FailureScreen : MonoBehaviour
+{
+    public GameObject failMenuUI;
+
+    void Start()
+    {
+        failMenuUI.SetActive(false);
+
+    }
+
+    public void FailScreen()
+    {
+        Time.timeScale = 0f;
+        failMenuUI.SetActive(true);
+    }
+
+    public void RetryLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+        Time.timeScale = 1f;
+    }
+}
