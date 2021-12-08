3364ef6a84ac960838b70483a760637d4f461c0e
Tommy Rennolds <thomasrennolds@gmail.com>
2021-12-08 13:36:13 -0500

    Revert "Revert "Success and Failure Screens added to SampleScene""
    This reverts commit e38ebdb4e7af6527826957d2409108ab10e3b490.

--- NewtonsFactory/Assets/Scripts/GameManager.cs
+++ NewtonsFactory/Assets/Scripts/GameManager.cs
@@ -5,16 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-  public void LevelFailed()
-  {
-    //player died or failed to reach peak velocity
-    Debug.Log("GAME OVER");
-    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-  }
+    public void LevelFailed()
+    {
+        //player died or failed to reach peak velocity
+        Debug.Log("GAME OVER");
+        FindObjectOfType<FailureScreen>().FailScreen();
+    }
 
-  public void LevelWon()
-  {
-    //player reached needed velocity
-    Debug.Log("YOU WON");
-  }
+    public void LevelWon()
+    {
+        //player reached needed velocity
+        Debug.Log("YOU WON");
+        FindObjectOfType<SuccessScreen>().WinScreen();
+    }
+
 }
